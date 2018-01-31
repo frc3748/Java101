62238671010b22503eec09b5aebf8c639fe03d81
Nick B <ambeloe@gmail.com>
2018-01-30 20:04:17 -0500

    Initial commit

---
+++ ButtonTesting/Button.java
@@ -0,0 +1,35 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinDirection;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.event.GpioPinListener;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.event.PinEventType;
+
+public class Button{
+public static void main(String[] args)throws InterruptedException{
+final GpioController gpio = GpioFactory.getInstance();
+	 GpioPinDigitalInput button = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04);
+	while(true){
+		boolean buttonState = button.isHigh();
+		if(buttonState){
+			System.out.println("Button State: On");
+		}else{
+			System.out.println("Button State: Off");
+		}
+		Thread.sleep(100);
+	}
+}
+}
