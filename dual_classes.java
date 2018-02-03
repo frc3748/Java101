9b75e8a64b56b240f97c0e8a4c83918cb1441fe0
Nick B <ambeloe@gmail.com>
2018-02-03 14:41:52 -0500

    wip class stuff

---
+++ dual_classes.java
@@ -0,0 +1,33 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinDirection;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.event.GpioPinListener;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.event.PinEventType;
+
+public class testbed{
+public static void main(String[] args){
+}
+}
+public class switchStuff{
+final GpioController gpio = GpioFactory.getInstance();
+public  static void setup(String pinName, String pin, String type){
+	if(type == "input"){
+	GpioPinDigitalInput pinName = gpio.provisionDigitalInputPin(RaspiPin.pin)
+}
+public static void state(){
+	
+}
